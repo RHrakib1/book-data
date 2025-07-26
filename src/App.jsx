335431ce30947252f93ff9ba6255710cc290aa04
rakib hasan <rakibhasanmd457@gmail.com>
2025-07-26 07:33:24 +0600

    router and full style setup and navbar import

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl bg-blue-500 font-bold text-blue-400">
+        Tailwind CSS is working!
+      </h1>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/Component/Body/Body.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Navbar from '../Navbar/Navbar'
+
+export default function Body() {
+  return (
+    <div>
+        <Navbar></Navbar>
+    </div>
+  )
+}
